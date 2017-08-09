97f05b1075ebc34d9e544446326e61eddffd36df
trisha94 <trishajha1994@gmail.com>
2017-08-09 12:00:54 +0530

    first

---
+++ spychat/calculator.py
@@ -0,0 +1,15 @@
+oper = input("Enter your operation first enter \n 1 = Add \n 2 = Subt \n 3 = Divide \n 4 = Multiply")
+num1 = input("enter your first number")
+num2 = input("enter your second number")
+if (oper == 1):
+    print ("Your answer is \n")
+    print (num1 + num2)
+elif(oper == 2):
+    print ("Your answer is \n")
+    print (num1 - num2)
+elif (oper == 3):
+    print ("Your answer is \n")
+    print (num1 / num2)
+elif (oper == 4):
+    print ("Your answer is \n")
+    print (num1 * num2)
--- spychat/main.py
+++ spychat/main.py
@@ -1,3 +1,7 @@
-print  "what\\\\'s up ? //\\\helloworld"
+
 name = raw_input("What is your name?")
 print ("welcome " +name)
+if(len(name)>0):
+    spy_salutation = raw_input("What should I call you")
+    print("Hello" +spy_salutation)
+
